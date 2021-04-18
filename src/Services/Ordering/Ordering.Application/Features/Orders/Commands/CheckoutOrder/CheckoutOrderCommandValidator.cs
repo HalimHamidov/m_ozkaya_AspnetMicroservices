e194dd874e51a4d9477efb47f4c679e95b39e7d0
Ibrahimhalilov, Ilham <ibrahimxelilov.i@gmail.com>
2021-04-19 01:16:44 +0300

    Ordering.Application CheckoutOrder command has been created

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/CheckoutOrder/CheckoutOrderCommandValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Commands.CheckoutOrder
+{
+    public class CheckoutOrderCommandValidator : AbstractValidator<CheckoutOrderCommand>
+    {
+        public CheckoutOrderCommandValidator()
+        {
+            RuleFor(p => p.UserName)
+                .NotEmpty().WithMessage("{UserName} is required.")
+                .NotNull().WithMessage("{UserName} is required.")
+                .MaximumLength(50).WithMessage("{UserName} must not exist 50 characters.");
+
+            RuleFor(p => p.EmailAddress)
+                .NotEmpty().WithMessage("{EmailAddress} is required.")
+                .NotNull().WithMessage("{EmailAddress} is required.");
+
+            RuleFor(p => p.TotalPrice)
+                .NotEmpty().WithMessage("{TotalPrice} is required.")
+                .NotNull().WithMessage("{TotalPrice} is required.")
+                .GreaterThan(0).WithMessage("{TotalPrice} should be greater than zero.");
+        }
+    }
+}
