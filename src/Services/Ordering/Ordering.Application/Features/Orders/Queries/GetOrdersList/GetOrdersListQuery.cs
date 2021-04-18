61e361058915abf8467e3d74d2dfb05da2878a6f
Ibrahimhalilov, Ilham <ibrahimxelilov.i@gmail.com>
2021-04-19 00:14:44 +0300

    Ordering.Application Orders Query has been added

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrdersListQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System.Collections.Generic;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrdersList
+{
+    public class GetOrdersListQuery : IRequest<List<OrdersVm>>
+    {
+        public string Username { get; set; }
+
+        public GetOrdersListQuery(string username)
+        {
+            Username = username;
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrdersListQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using MediatR;
+using Ordering.Application.Contracts.Persistence;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrdersList
+{
+    public class GetOrdersListQueryHandler : IRequestHandler<GetOrdersListQuery, List<OrdersVm>>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+
+        public GetOrdersListQueryHandler(IOrderRepository orderRepository, IMapper mapper)
+        {
+            _orderRepository = orderRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<OrdersVm>> Handle(GetOrdersListQuery request,
+            CancellationToken cancellationToken)
+        {
+            var orderList = await _orderRepository.GetByUserNameAsync(request.Username);
+            return _mapper.Map<List<OrdersVm>>(orderList);
+        }
+    }
+}
