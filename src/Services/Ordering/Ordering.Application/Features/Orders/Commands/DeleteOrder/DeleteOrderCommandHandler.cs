ba262a5772023856cecc6c155228432c1f3a1ad9
Ilham Ibrahimkhalilov <ibrahimxelilov.i@gmail.com>
2021-04-20 13:08:38 +0300

    Ordering.Application Exceptions folder and custom exceptions handling added

--- src/Services/Ordering/Ordering.Application/Features/Orders/Commands/DeleteOrder/DeleteOrderCommandHandler.cs
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/DeleteOrder/DeleteOrderCommandHandler.cs
@@ -2,6 +2,8 @@
 using MediatR;
 using Microsoft.Extensions.Logging;
 using Ordering.Application.Contracts.Persistence;
+using Ordering.Application.Exceptions;
+using Ordering.Domain.Entities;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -25,7 +27,7 @@ namespace Ordering.Application.Features.Orders.Commands.DeleteOrder
             var orderToDelete = await _orderRepository.GetByIdAsync(request.Id);
             if (orderToDelete == null)
             {
-                _logger.LogError("Order not exists on database.");
+                //_logger.LogError("Order not exists on database.");
                 throw new NotFoundException(nameof(Order), request.Id);
             }
 
